2ee05b205f1a73fd48c151388f0202d65abaa1d4
Jhoan-pro <jdmunozm@unimayor.edu.co>
2026-06-02 01:50:23 -0500

    creacion de entorno para la app mobile

---
+++ Momentum_native/momentum-mobile/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "#07111f", alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ color: "white" }}>Momentum 🚀</Text>
+    </View>
+  );
+}
